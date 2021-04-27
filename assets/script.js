82c6ef441f0b24b3fc7be018b47d90befd986cbf
lapape <lapape07@gmail.com>
2021-04-27 17:31:35 -0600

    changed from random questions to incrementing through each question, made final score screen

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,10 @@ var introScreen = document.querySelector("#intro");
 var timerScreen = document.querySelector("#timer");
 var questionScreen = document.querySelector("#questions-screen");
 var timeUpScreen = document.querySelector("#time-up");
+var scoreScreen = document.querySelector("#score-screen");
+var scoreButton = document.querySelector("#initials-button");
 var finalScreen = document.querySelector("#final-screen");
+var finalScoreDisplay = document.querySelector("#final-score");
 var startButton = document.querySelector("#start-button");
 var timeUpButton = document.querySelector("#time-up-button");
 var userResponse = document.querySelector("#response");
@@ -12,6 +15,8 @@ var option1 = document.querySelector("#answer-1");
 var option2 = document.querySelector("#answer-2");
 var option3 = document.querySelector("#answer-3");
 var option4 = document.querySelector("#answer-4");
+var initialDisplay = document.querySelector("#initial-display");
+
 var randomQuestion;
 var userAnswer;
 var playerScore = 0;
@@ -69,42 +74,70 @@ var quiz = [
     asked: false,
   },
 ];
-
+var currentQuestion = quiz[0];
+var index = quiz.indexOf(currentQuestion);
 startButton.addEventListener("click", startGame);
 // timeUpButton.addEventListener("click", renderScore);
 submitButton.addEventListener("click", checkAnswer);
+// scoreButton.addEventListener("click", displayScore);
 
 function startGame() {
   //start timer
   introScreen.style.display = "none";
   questionScreen.style.display = "block";
 
-  randomQuestion = quiz[Math.floor(Math.random() * quiz.length)];
-  questionText.textContent = randomQuestion.question;
-  option1.textContent = randomQuestion.answers[0];
-  option2.textContent = randomQuestion.answers[1];
-  option3.textContent = randomQuestion.answers[2];
-  option4.textContent = randomQuestion.answers[3];
-  randomQuestion.asked = true;
+  // randomQuestion = quiz[Math.floor(Math.random() * quiz.length)];
+  questionText.textContent = currentQuestion.question;
+  option1.textContent = currentQuestion.answers[0];
+  option2.textContent = currentQuestion.answers[1];
+  option3.textContent = currentQuestion.answers[2];
+  option4.textContent = currentQuestion.answers[3];
+  // randomQuestion.asked = true;
 }
 
 function checkAnswer() {
-  var userAnswer = randomQuestion.answers[userResponse.selectedIndex - 1];
+  if (index + 1 === quiz.length) {
+    renderFinalScore();
+  }
+  var userAnswer = currentQuestion.answers[userResponse.selectedIndex - 1];
   console.log(userAnswer);
-  console.log(randomQuestion.correctAnswer);
-  if (userAnswer === randomQuestion.correctAnswer) {
+  console.log(currentQuestion.correctAnswer);
+  if (userAnswer === currentQuestion.correctAnswer) {
     playerScore++;
   } else {
     timeLeft -= 5;
   }
+  index++;
+  currentQuestion = quiz[index];
   console.log(playerScore);
-  do {
-    randomQuestion.asked = true;
-    randomQuestion = quiz[Math.floor(Math.random() * quiz.length)];
-    questionText.textContent = randomQuestion.question;
-    option1.textContent = randomQuestion.answers[0];
-    option2.textContent = randomQuestion.answers[1];
-    option3.textContent = randomQuestion.answers[2];
-    option4.textContent = randomQuestion.answers[3];
-  } while (randomQuestion.asked === false);
+  console.log(index);
+  questionText.textContent = currentQuestion.question;
+  option1.textContent = currentQuestion.answers[0];
+  option2.textContent = currentQuestion.answers[1];
+  option3.textContent = currentQuestion.answers[2];
+  option4.textContent = currentQuestion.answers[3];
+
+  // do {
+  //   randomQuestion = quiz[Math.floor(Math.random() * quiz.length)];
+  //   questionText.textContent = randomQuestion.question;
+  //   option1.textContent = randomQuestion.answers[0];
+  //   option2.textContent = randomQuestion.answers[1];
+  //   option3.textContent = randomQuestion.answers[2];
+  //   option4.textContent = randomQuestion.answers[3];
+  //   randomQuestion.asked = true;
+  // } while (randomQuestion.asked === false);
+  // renderFinalScore();
 }
+
+function renderFinalScore() {
+  timeUpScreen.style.display = "none";
+  questionScreen.style.display = "none";
+  finalScreen.style.display = "block";
+  finalScoreDisplay.textContent = "Final Score: " + playerScore;
+}
+
+// function displayScore() {
+//   finalScreen.style.display = "none";
+//   scoreScreen.style.display = "block";
+//   initialDisplay.textContent =
+// }
